011aa492118dafc541c632e36af77396176e6e30
JJRomeroCruz <juanjorcr98@gmail.com>
2026-04-02 13:47:40 +0200

    config and requiremts

---
+++ config.py
@@ -0,0 +1,16 @@
+GROUP_NAME = "TALENTOSOS"
+
+USERS = [
+    "Julio",
+    "Edu",
+    "Verdín",
+    "Bal tosar",
+    "Carlos", 
+    "Pablo",
+    "Luis",
+    "Cuoc",
+    "Juano",
+]
+
+MIN_NUMBER = 0
+MAX_NUMBER = 7
